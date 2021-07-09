668cca7c9d99717c67d534b11d5e87acd3aeadf5
Daniela Gimeno <daniela.gimeno@accuity.com>
2021-07-09 18:07:05 -0300

    hero digital exam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hero-app';
+  title = 'Sign up for email updates';
+  digitalForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  get f() {
+    return this.digitalForm && this.digitalForm.controls;
+  }
+
+  createForm() {
+    this.digitalForm = this.fb.group({
+       firstName: ['', Validators.required ],
+       lastName: ['', Validators.required ],
+       eMail: ['', Validators.required],
+       euResident: ['', Validators.required ],
+       organization:[''],
+       advances:[''],
+       alerts:[''],
+       otherCom:['']
+    });
+  }
 }
